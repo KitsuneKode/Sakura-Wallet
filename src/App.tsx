0ad20b0ffdd5656557521ca80b8a8feb7b150f9a
KitsuneKode <bhuyanmanash2002@gmail.com>
2024-08-30 03:10:34 +0530

    removed load animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { useState } from "react";
-import LoadingAnimation from "./components/LoadingAnimation";
+// import { useState } from "react";
+// import LoadingAnimation from "./components/LoadingAnimation";
 import LandingPage from "./components/Landing";
 import Component from "./components/Dashboard";
 import NotFound from "./components/404";
@@ -10,23 +10,23 @@ import { SpeedInsights } from "@vercel/speed-insights/react";
 import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
-  const [isLoading, setIsLoading] = useState(true);
-  const loadingDuration = 4000; // 4 seconds
+  // const [isLoading, setIsLoading] = useState(true);
+  // const loadingDuration = 4000; // 4 seconds
 
-  if (isLoading) {
-    return (
-      <LoadingAnimation
-        duration={loadingDuration}
-        onLoadingComplete={() => {
-          if (document.readyState === "complete") {
-            setIsLoading(false);
-          } else {
-            window.location.reload();
-          }
-        }}
-      />
-    );
-  }
+  // if (isLoading) {
+  //   return (
+  //     <LoadingAnimation
+  //       duration={loadingDuration}
+  //       onLoadingComplete={() => {
+  //         if (document.readyState === "complete") {
+  //           setIsLoading(false);
+  //         } else {
+  //           window.location.reload();
+  //         }
+  //       }}
+  //     />
+  //   );
+  // }
 
   return (
     <>
